bd26a13a1785c19e9d2003ffb331d6096c14deda
Evgenij-Evgenij <evgenijvoronov150@gmail.com>
2022-12-31 11:56:48 +0200

    Initial Commit

---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by Евгений Воронов on 31.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
