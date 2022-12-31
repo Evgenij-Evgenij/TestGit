5cd457bf079d72619d952a8738b87a9b7f5685e9
Evgenij-Evgenij <evgenijvoronov150@gmail.com>
2022-12-31 12:42:19 +0200

    1

---
+++ TestGit/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  TestGit
+//
+//  Created by Евгений Воронов on 31.12.2022.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Screen Two")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
